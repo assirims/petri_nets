aa12b86ec76203c8b020a142c3a26eeddf8e41a7
Marcin <spiewak.marc@gmail.com>
2015-05-01 14:39:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# petri_nets
---
+++ components.py
@@ -0,0 +1,44 @@
+import uuid
+
+
+class Direction(object):
+    PLACE_TO_TRANSITION = 1
+    TRANSITION_TO_PLACE = 2
+
+
+class Place(object):
+    def __init__(self, name=uuid.uuid4().get_hex(), id=None, tokens=0):
+        self.id = id
+        self.name = name
+        self.tokens = tokens
+
+
+class Transition(object):
+    def __init__(self, connectors_in, connectors_out, priority, id=None, name=uuid.uuid4().get_hex()):
+        self.id = id
+        self.name = name
+        self.connectors_in = connectors_in
+        self.connectors_out = connectors_out
+        self.priority = priority
+
+    # check that you can execute transition
+    def __is_doable(self):
+        for connector in self.connectors_in:
+            if connector.weight > connector.place.tokens:
+                return False
+        return True
+
+    def run_transition(self):
+        if self.__is_doable():
+            for connector_in in self.connectors_in:
+                connector_in.place.tokens = connector_in.place.tokens - connector_in.weight
+
+            for connector_out in self.connectors_out:
+                connector_out.place.tokens = connector_out.place.tokens + connector_out.weight
+
+
+class Connector(object):
+    def __init__(self, place, direction, weight):
+        self.place = place
+        self.direction = direction
+        self.weight = weight
